15c92ec8241b3f0711ac9096ac6fe24e63830ddc
simon <kausi@heka.or.at>
2019-06-28 21:12:50 +0100

    Added code for State class

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,15 @@
+import java.util.ArrayList;
 
 public class Game {
 	
 	int player;
 	Model model;
+	ArrayList<State> states;
 	
 	public Game(int player, Model m) {
 		this.player = player;
 		this.model = m;
+		this.states = new ArrayList<State>();
 	}
 	
 	public boolean otherPlayer(int field) {
--- src/State.java
+++ src/State.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class State {
 	
@@ -5,6 +6,70 @@ public class State {
 	
 	public State(Integer[] board) {
 		this.board = board;
+	}
+	
+	public ArrayList<State> nextSteps(int player) {
+		ArrayList<State> next = new ArrayList<State>();
+		
+		for (int i = 0; i < board.length; i++) {
+			if (board[i] == 0) {
+				Integer[] new_state = board.clone();
+				new_state[i] = player;
+				next.add(new State(new_state));
+			}
+		}
+		
+		return next;
+	}
+	
+	private boolean win(int player) {
+		for (int i = 0; i < 3; i++) {
+			// horizontal:
+			if (board[3 * i + 0] == player &&
+				board[3 * i + 1] == player &&
+				board[3 * i + 2] == player) {
+				return true;
+			}
+			// vertical:
+			if (board[0 + i] == player &&
+				board[3 + i] == player &&
+				board[6 + i] == player) {
+				return true;
+			}
+		}
+		// diagonal:
+		if (board[0] == player &&
+			board[4] == player &&
+			board[8] == player) {
+			return true;
+		}
+		if (board[2] == player &&
+			board[4] == player &&
+			board[6] == player) {
+			return true;
+		}
+		return false;
+	}
+	
+	public int result() {
+		int res = 0; // draw
+		
+		for (int i = 0; i < 9; i++) {
+			if (board[i] == 0) {
+				res = -1; // not decided yet
+			}
+		}
+		
+		if (win(1))
+			res = 1; // player 1 wins
+		else if (win(2))
+			res = 2; // player 2 wins
+		
+		return res;
+	}
+	
+	public Integer[] get() {
+		return this.board;
 	}
 
 }
