da15272ad61ef75df4c1568caf8a5d50fa0242b0
Simon <kausi@heka.or.at>
2019-07-02 20:55:01 +0100

    Added frontend react files

---
+++ frontend/tic-tac-toe/src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import 'typeface-roboto';
+
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Tic Tac Toe
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <ButtonAppBar/>
+    </div>
+  );
+}
+
+export default App;
