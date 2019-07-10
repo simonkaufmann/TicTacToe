ffa8adb4e58036797a5573cbe828fe251bf6cfe2
simon <kausi@heka.or.at>
2019-07-10 23:37:43 +0100

    Getting body of post request works

--- src/tic_tac_toe/Socket.java
+++ src/tic_tac_toe/Socket.java
@@ -1,11 +1,16 @@
 package tic_tac_toe;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
 
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 public class Socket {
@@ -22,31 +27,103 @@ public class Socket {
 		}
 	}
 	
-	private static void startGame(HttpExchange exchange) throws IOException {
-		String response = "OK";
-		exchange.getResponseHeaders().add("Content-type", "application/json");
-		exchange.sendResponseHeaders(200, response.getBytes().length);
-		OutputStream os = exchange.getResponseBody();
-		os.write(response.getBytes());
-		os.close();
+	static class ApiHandler implements HttpHandler {
+
+	    private final TicTacToe ticTac;
+
+	    public ApiHandler(TicTacToe m) {
+	        ticTac = m;
+	    }
+	    
+		private void startGame(HttpExchange exchange) throws IOException {
+			ticTac.startGame();
+			
+			String response = "OK";
+			exchange.getResponseHeaders().add("Content-type", "application/json");
+			exchange.sendResponseHeaders(200, response.getBytes().length);
+			OutputStream os = exchange.getResponseBody();
+			os.write(response.getBytes());
+			os.close();
+		}
+		
+		private void sendMove(HttpExchange exchange) throws IOException{
+			InputStream in = exchange.getRequestBody();
+			String body = inputStreamToString(in);
+			System.out.println("body: " + body);
+			
+			String response = "OK";
+			exchange.getResponseHeaders().add("Content-type", "application/json");
+			exchange.sendResponseHeaders(200, response.getBytes().length);
+			OutputStream os = exchange.getResponseBody();
+			os.write(response.getBytes());
+			os.close();
+		}
+		
+		private void getMove(HttpExchange exchange) throws IOException {
+			String response = "OK";
+			exchange.getResponseHeaders().add("Content-type", "application/json");
+			exchange.sendResponseHeaders(200, response.getBytes().length);
+			OutputStream os = exchange.getResponseBody();
+			os.write(response.getBytes());
+			os.close();
+		}
+		
+		private void defaultHandler(HttpExchange exchange) throws IOException {
+			String response = "Path not recognised";
+			exchange.sendResponseHeaders(404, response.getBytes().length);
+			OutputStream os = exchange.getResponseBody();
+			os.write(response.getBytes());
+			os.close();
+		}
+
+		@Override
+	    public void handle(HttpExchange exchange) throws IOException {    	
+    		String stringUri = exchange.getRequestURI().toString();
+    		Uri uri = parseUri(stringUri);
+
+    		String[] path = uri.path;
+
+    		// path[0] is always "api" because handler is set for it
+    		
+    		if (path.length >= 3) {
+    			switch (path[2]) {
+    				case "start-game":
+    					startGame(exchange);
+    					break;
+    				case "send-move":
+    					sendMove(exchange);
+    					break;
+    				case "get-move":
+    					getMove(exchange);
+    					break;
+    				default:
+    					defaultHandler(exchange);
+    			}
+    		}
+	    }
 	}
 	
-	private static void sendMove(HttpExchange exchange) throws IOException{
-		String response = "OK";
-		exchange.getResponseHeaders().add("Content-type", "application/json");
-		exchange.sendResponseHeaders(200, response.getBytes().length);
-		OutputStream os = exchange.getResponseBody();
-		os.write(response.getBytes());
-		os.close();
-	}
+	private static String inputStreamToString(InputStream t) {
+		try {
+			InputStreamReader isr;
+			isr = new InputStreamReader(t,"utf-8");
+			BufferedReader br = new BufferedReader(isr);
 	
-	private static void getMove(HttpExchange exchange) throws IOException {
-		String response = "OK";
-		exchange.getResponseHeaders().add("Content-type", "application/json");
-		exchange.sendResponseHeaders(200, response.getBytes().length);
-		OutputStream os = exchange.getResponseBody();
-		os.write(response.getBytes());
-		os.close();
+			int b;
+			StringBuilder buf = new StringBuilder(512);
+			while ((b = br.read()) != -1) {
+			    buf.append((char) b);
+			}
+	
+			br.close();
+			isr.close();
+			return buf.toString();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "";
 	}
 	
 	private static Uri parseUri(String uri) {
@@ -58,7 +135,7 @@ public class Socket {
 			fragment = fragment + split[i];
 		}
 		
-		split = uri.split("?");
+		split = uri.split("\\?");
 		String path = split[0];
 		String query = "";
 		for (int i = 1; i < split.length; i++) {
@@ -70,35 +147,12 @@ public class Socket {
 		Uri parsed = new Uri(splitPath, query, fragment);
 		return parsed;
 	}
-	
-	private static void handleRequest(HttpExchange exchange) throws IOException {
-		String stringUri = exchange.getRequestURI().toString();
-		Uri uri = parseUri(stringUri);
 
-		String[] path = uri.path;
-		
-		// path[0] is always "api" because handler is set for it
-		
-		if (path.length >= 2) {
-			switch (path[1]) {
-				case "start-game":
-					startGame(exchange);
-					break;
-				case "send-move":
-					sendMove(exchange);
-					break;
-				case "get-move":
-					getMove(exchange);
-					break;
-			}
-		}
-	}
-
-	public void startServer() {
+	public void startServer(TicTacToe ticTac) {
 		try {
 			HttpServer server = HttpServer.create(new InetSocketAddress(3001), 0);
 			HttpContext api = server.createContext("/api");
-			api.setHandler(Socket::handleRequest);
+			api.setHandler(new ApiHandler(ticTac));
 			server.start();
 		} catch (IOException ex) {
 			System.err.println(ex.getMessage());
--- src/tic_tac_toe/TicTacToe.java
+++ src/tic_tac_toe/TicTacToe.java
@@ -1,10 +1,22 @@
 package tic_tac_toe;
 
+import tic_tac_toe.Game;
+
 public class TicTacToe {
 	
+	private Game game;
+	
 	public static void main(String[] args) {
+		TicTacToe ticTac = new TicTacToe();
+		ticTac.startGame();
+		
 		Socket soc = new Socket();
-		soc.startServer();
+		soc.startServer(ticTac);
+	}
+	
+	public void startGame() {
+		Model m = new Model();
+		game = new Game(2, m);
 	}
 
 }
