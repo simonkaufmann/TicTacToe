3adf8ce5b08502ae43fac7177e47244e5533aa7d
Simon <kausi@heka.or.at>
2019-07-12 20:19:03 +0100

    TextFit for the X and maxwidth for tic tac toe board

--- frontend/tic-tac-toe/src/Home.js
+++ frontend/tic-tac-toe/src/Home.js
@@ -8,6 +8,8 @@ import Button from '@material-ui/core/Button';
 
 import ButtonAppBar from './ButtonAppBar.js';
 
+var ReactFitText = require('react-fittext');
+
 const useStyles = makeStyles(theme => ({
   button: {
     margin: theme.spacing(1),
@@ -29,7 +31,9 @@ class Square extends React.Component {
           className="square"
           onClick={() => this.setState({value: 'X'})}
         >
-          {this.state.value}
+          <ReactFitText compressor={0.25}>
+          <h1>{this.state.value}</h1>
+          </ReactFitText>
         </button>
       </div>
     );
@@ -42,27 +46,27 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
-
     return (
-      <div className="board-box">
-        <table className="board">
-          <tr className="board-row"><td className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </td></tr>
-          <tr className="board-row"><td className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </td></tr>
-          <tr className="board-row"><td className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </td></tr>
-        </table>
+      <div className="board-outer-box">
+        <div className="board-box">
+          <table className="board">
+            <tr className="board-row"><td className="board-row">
+              {this.renderSquare(0)}
+              {this.renderSquare(1)}
+              {this.renderSquare(2)}
+            </td></tr>
+            <tr className="board-row"><td className="board-row">
+              {this.renderSquare(3)}
+              {this.renderSquare(4)}
+              {this.renderSquare(5)}
+            </td></tr>
+            <tr className="board-row"><td className="board-row">
+              {this.renderSquare(6)}
+              {this.renderSquare(7)}
+              {this.renderSquare(8)}
+            </td></tr>
+          </table>
+        </div>
       </div>
     );
   }
