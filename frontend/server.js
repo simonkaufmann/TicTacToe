aa3b2246f2b5554de80ad9b0eee7a85d94e80041
Simon Kaufmann <simon.k1996@gmail.com>
2019-07-29 02:20:58 +0100

    fixed server.js

--- frontend/server.js
+++ frontend/server.js
@@ -11,7 +11,10 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.use('/api', proxy(process.env.BACKEND_URL + '/api'));
+app.use('/api', proxy('localhost:3001', {
+  proxyReqPathResolver: function (req) {
+    return '/api' + req.url;
+  }}));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
