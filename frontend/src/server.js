15336c1706bec1fd3af736025a06367c7f016b15
Simon Kaufmann <simon.k1996@gmail.com>
2019-07-29 01:23:17 +0100

    fixes for server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
+const proxy = require('express-http-proxy');
 const port = process.env.PORT || 8080;
 const app = express();
 
